d021ed79cfe2278455ff070da71fab54bf9177f3
Ya-Sin <ya-sin@Ya-Sin.Ya-Sin>
2026-05-17 21:42:58 -0400

    Added Menu Endpoint2

--- backend_api/Controllers/Menu.cs
+++ backend_api/Controllers/Menu.cs
@@ -1,19 +1,31 @@
-[HttpGet("menu")]
-public IActionResult GetProfileMenu()
-{
-    var menu = new[]
-    {
-        new
-        {
-            title = "الملف الشخصي",
-            route = "/admin/profile"
-        },
-        new
-        {
-            title = "الإعدادات",
-            route = "/admin/settings"
-        }
-    };
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
-    return Ok(menu);
+namespace CarMaintenance.Controllers
+{
+    [ApiController]
+    [Route("api/menu")]
+    [Authorize]
+    public class MenuController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMenu()
+        {
+            var menu = new[]
+            {
+                new
+                {
+                    title = "الملف الشخصي",
+                    route = "/admin/profile"
+                },
+                new
+                {
+                    title = "الإعدادات",
+                    route = "/admin/settings"
+                }
+            };
+
+            return Ok(menu);
+        }
+    }
 }
